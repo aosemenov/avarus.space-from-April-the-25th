70bd3a48162ecf29174fd3b3ffc5d68bded25611
Артем Семенов <aosemenov_1@miem.hse.ru>
2021-04-26 01:05:21 +0300

    srpit fixs

--- scriptLibrariesInstallForRlang.r
+++ scriptLibrariesInstallForRlang.r
@@ -3,4 +3,4 @@ install.packages("openxlsx", repos="http://cran.us.r-project.org")
 install.packages("readxl", repos="http://cran.us.r-project.org")
 install.packages("clusteval", repos="http://cran.us.r-project.org")
 install.packages("vegan", repos="http://cran.us.r-project.org")
-print('The End 777 of the R Libraries Installation Script. (maybe unsuccessful)'
+print('The End 777 of the R Libraries Installation Script. (maybe unsuccessful)')
