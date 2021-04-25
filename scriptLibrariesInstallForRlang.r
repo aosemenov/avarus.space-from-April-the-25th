30dea8d77231258446e5f4c0a40231eed9a9c223
Артем Семенов <aosemenov_1@miem.hse.ru>
2021-04-25 22:57:28 +0300

    fix_db3

--- scriptLibrariesInstallForRlang.r
+++ scriptLibrariesInstallForRlang.r
@@ -1,5 +1,4 @@
 install.packages("xtable", repos="http://cran.us.r-project.org")
 install.packages("openxlsx", repos="http://cran.us.r-project.org")
 install.packages("readxl", repos="http://cran.us.r-project.org")
-install.packages("cluster", repos="http://cran.us.r-project.org")
 print('The End of the R Libraries Installation Script. (maybe unsuccessful')
